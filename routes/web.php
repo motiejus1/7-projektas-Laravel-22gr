97f79d8e20c0bcb001c6d7b05ba14d941cedcfb5
motiejus1 <motiejus.baliukonis@vka.lt>
2022-01-28 19:18:08 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('profileimages')->group(function() {
+
+    // Route::get('', 'App\Http\Controllers\StudentController@index')->name('student.index');
+    Route::get('create', 'App\Http\Controllers\ProfileImageController@create')->name('profileimage.create');
+    // Route::post('store', 'App\Http\Controllers\StudentController@store' )->name('student.store');
+    // Route::get('edit/{student}', 'App\Http\Controllers\StudentController@edit')->name('student.edit');
+    // Route::post('update/{student}', 'App\Http\Controllers\StudentController@update')->name('student.update');
+    // Route::post('destroy/{company}', 'App\Http\Controllers\CompanyController@destroy' )->name('company.destroy');
+    // Route::get('show/{student}', 'App\Http\Controllers\StudentController@show')->name('student.show');
+
+});
